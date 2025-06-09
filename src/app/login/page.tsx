a1b942caba842262f0f4d2d375eba5d3755180ab
sanghun lee <57477415+sanghunlee-711@users.noreply.github.com>
2025-06-09 11:15:34 +0900

    feat: initial UI skeleton with shadcn style

---
+++ src/app/login/page.tsx
@@ -0,0 +1,8 @@
+export default function LoginPage() {
+  return (
+    <div className="flex flex-col items-center justify-center h-[60vh] gap-4">
+      <h1 className="text-2xl font-bold">Login</h1>
+      {/* TODO: login form */}
+    </div>
+  );
+}
---
+++ src/app/posts/page.tsx
@@ -0,0 +1,8 @@
+export default function PostsPage() {
+  return (
+    <div className="p-4">
+      <h1 className="text-xl font-bold mb-4">Posts</h1>
+      {/* TODO: posts list */}
+    </div>
+  );
+}
