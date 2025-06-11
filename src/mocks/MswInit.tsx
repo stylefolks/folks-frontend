f3c315dabb5bba1fb048262f51b28e725336b059
sanghun lee <57477415+sanghunlee-711@users.noreply.github.com>
2025-06-12 08:38:58 +0900

    Add MSW service worker and update startup

--- src/mocks/MswInit.tsx
+++ src/mocks/MswInit.tsx
@@ -5,7 +5,12 @@ export default function MswInit() {
   useEffect(() => {
     if (process.env.NEXT_PUBLIC_API_MOCKING === 'enabled') {
       import('./browser').then(({ worker }) => {
-        worker.start();
+        worker.start({
+          serviceWorker: {
+            url: '/mockServiceWorker.js',
+          },
+          onUnhandledRequest: 'bypass',
+        });
       });
     }
   }, []);
