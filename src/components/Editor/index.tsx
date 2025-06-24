344abc97f9d3311caaaaca2af19acddc9dec2c50
sanghun lee <57477415+sanghunlee-711@users.noreply.github.com>
2025-06-24 15:14:07 +0900

    Add crew mention suggestions in editor (#80)

--- src/components/Editor/index.tsx
+++ src/components/Editor/index.tsx
@@ -3,23 +3,25 @@ import { Node } from "prosemirror-model";
 import { EditorState } from "prosemirror-state";
 import { EditorView } from "prosemirror-view";
 import { editorSchema } from "@/lib/editorSchema";
-import "../Editor/core/editor.css"
+import "../Editor/core/editor.css";
 import { setup } from "./core";
 
 interface EditorProps {
   value: object;
   onChange: (value: EditorState) => void;
+  onReady?: (view: EditorView) => void;
 }
 
-export const Editor: React.FC<EditorProps> = ({ value , onChange}: EditorProps) => {
+export const Editor: React.FC<EditorProps> = ({
+  value,
+  onChange,
+  onReady,
+}: EditorProps) => {
   const ref = useRef<HTMLDivElement>(null);
   const editorRef = useRef<EditorView | null>(null);
-  
 
   useEffect(() => {
-    if(typeof window === 'undefined') return; 
-
-
+    if (typeof window === "undefined") return;
 
     if (ref.current !== null && !editorRef.current) {
       const schema = editorSchema;
@@ -27,17 +29,21 @@ export const Editor: React.FC<EditorProps> = ({ value , onChange}: EditorProps) 
       const plugins = setup({ schema });
       const state = EditorState.create({ doc, plugins });
 
-      editorRef.current = new EditorView(ref.current, { state , dispatchTransaction:(transaction)=> {
-        if (editorRef.current) {
-          const newState = editorRef.current.state.apply(transaction);
-          editorRef.current.updateState(newState);
-          onChange(newState);
-        }
-      }});
+      editorRef.current = new EditorView(ref.current, {
+        state,
+        dispatchTransaction: (transaction) => {
+          if (editorRef.current) {
+            const newState = editorRef.current.state.apply(transaction);
+            editorRef.current.updateState(newState);
+            onChange(newState);
+          }
+        },
+      });
+      if (onReady && editorRef.current) {
+        onReady(editorRef.current);
+      }
     }
-
   }, [value]);
 
-  return <div  ref={ref} className="editor-wrapper" />;
-}
-
+  return <div ref={ref} className="editor-wrapper" />;
+};
---
+++ src/constants/meta.ts
@@ -0,0 +1,9 @@
+export enum CrewMetaType {
+  EVENT = 'EVENT',
+  NOTICE = 'NOTICE',
+}
+
+export enum BrandMetaType {
+  PROMOTION = 'PROMOTION',
+  NOTICE = 'NOTICE',
+}
