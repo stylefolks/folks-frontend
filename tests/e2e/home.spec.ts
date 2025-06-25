307b9999d184f8b12547bb9346a3d0b78646afcd
sanghunlee-711 <cloudlee711@gmail.com>
2025-06-25 09:55:06 +0900

    chore: typo in testcode

--- tests/e2e/home.spec.ts
+++ tests/e2e/home.spec.ts
@@ -2,5 +2,5 @@ import { test, expect } from '@playwright/test';
 
 test('home page displays navbar brand', async ({ page }) => {
   await page.goto('/');
-  await expect(page.getByText('Stylefolks')).toBeVisible();
+  await expect(page.getByText('Folks')).toBeVisible();
 });
